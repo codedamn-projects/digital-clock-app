b6bc9ee68844f99d5640416c620f8fd1d9afba98
Pranav <pranav.techiegeek@gmail.com>
2022-11-11 18:33:27 +0530

    boilerplate reop

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Web Clock Starter!</h1>
+      <h2>Good Luck</h2>
+    </div>
+  );
+}
+
+export default App;
